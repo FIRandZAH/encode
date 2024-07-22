b0d836a403a8fd12f2d6bc706a96c84b18c1f573
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-07-22 22:31:14 +0700

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,7 +1,9 @@
 # mau ngapain mau recode?
 # yahh sorry ga bisa
 # salam dari firman 😎
+import os
 import subprocess
+os.system('git pull')
 def run_script(script_path):
         subprocess.run(['python', script_path], check=True)
 script_path = 'donghua.py'
