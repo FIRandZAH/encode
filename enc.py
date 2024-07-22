79a843e51adf1bec3039efee7c371790590f235a
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-07-22 22:32:31 +0700

    Update enc.py

--- enc.py
+++ enc.py
@@ -6,5 +6,5 @@ import subprocess
 os.system('git pull')
 def run_script(script_path):
         subprocess.run(['python', script_path], check=True)
-script_path = 'donghua.py'
+script_path = 'edan.py'
 run_script(script_path)
