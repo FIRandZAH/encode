68fc55f393764d7e2128be3ea6ab7683ecdd3661
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-07-22 10:58:29 +0700

    Create enc.py

---
+++ enc.py
@@ -0,0 +1,8 @@
+# mau ngapain mau recode?
+# yahh sorry ga bisa
+# salam dari firman 😎
+import subprocess
+def run_script(script_path):
+        subprocess.run(['python', script_path], check=True)
+script_path = 'donghua.py'
+run_script(script_path)
