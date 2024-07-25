69614742c6bd7b0f6a3ecd6d129a01b11cc85cb1
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-07-25 08:39:15 +0700

    Update decode.py

--- decode.py
+++ decode.py
@@ -3,8 +3,20 @@
 # salam dari firman 😎
 import os
 import subprocess
-os.system('git pull')
-def run_script(script_path):
-        subprocess.run(['python', script_path], check=True)
-script_path = 'asu.py'
-run_script(script_path)
+import sys
+def check_python_version():
+    python_available = subprocess.run(['which', 'python'], capture_output=True, text=True)
+    if python_available.returncode == 0:
+        return 'python'
+    python3_available = subprocess.run(['which', 'python3'], capture_output=True, text=True)
+    if python3_available.returncode == 0:
+        return 'python3'
+def run_script(script_path, python_executable):
+    subprocess.run([python_executable, script_path], check=True)
+def main():
+    os.system('git pull')
+    python_executable = check_python_version()
+    script_path = 'asu.py'
+    run_script(script_path, python_executable)
+if __name__ == "__main__":
+    main()
