f8f06b8451a6533ef668650a8d66552636503eb4
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-07-25 08:49:00 +0700

    Update decode.py

--- decode.py
+++ decode.py
@@ -5,12 +5,18 @@ import os
 import subprocess
 import sys
 def check_python_version():
-    python_available = subprocess.run(['which', 'python'], capture_output=True, text=True)
-    if python_available.returncode == 0:
+    try:
+        subprocess.run(['python', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return 'python'
-    python3_available = subprocess.run(['which', 'python3'], capture_output=True, text=True)
-    if python3_available.returncode == 0:
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        pass
+    try:
+        subprocess.run(['python3', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return 'python3'
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        pass
+    print("Python atau Python3 tidak ditemukan di sistem.")
+    sys.exit(1)
 def run_script(script_path, python_executable):
     subprocess.run([python_executable, script_path], check=True)
 def main():
