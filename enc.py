f360b862878d28d3b52d19998a8e8783529bc0e1
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-08-08 09:50:01 +0700

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,4 +1,10 @@
-# pake aja
-# ga usah di apa-apain
-# capek coding 😑🔪
-import data
+# mau ngapain
+# pake aja ga usah
+# di apa-apain
+# capek coding 😑🔪🔪🔪
+import os, sys
+os.system('git pull')
+try:
+    __import__("data").main()
+except Exception as e:
+    exit(str(e))
