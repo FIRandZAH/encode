a24f8ff4316246c8b7641f7adc264fe6d817ee41
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-08-25 09:43:58 +0700

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,1 +1,9 @@
+# mau ngapain
+# pake aja ga usah
+# di apa-apain
+# capek coding 😑🔪🔪🔪
+# nih enc selain enc cython
+# ada 400 lapis enc lainnya
+# jadi ga usah di dec
+# kalo gabut yah silahkan
 import decode
