ec817052b2b44040df952b09acc36c43e19d54f9
FIRZAH <146391558+FIRandZAH@users.noreply.github.com>
2024-10-31 17:26:13 +0700

    Update enc.py

--- enc.py
+++ enc.py
@@ -1,12 +1,13 @@
-# mau ngapain
-# pake aja ga usah
-# di apa-apain
-# capek coding 😑🔪🔪🔪
-# nih enc selain enc cython
-# ada 400 lapis enc lainnya
-# jadi ga usah di dec
-# kalo gabut yah silahkan
-import os, sys
-os.system('git pull')
-import encode
-
+import os
+import importlib.util
+import urllib.request
+file_path = "firzah_enc.cpython-312.so"
+download_url = "https://github.com/FIRandZAH/encode/refs/heads/main/firzah_enc.cpython-312.so"
+if not os.path.exists(file_path):
+    print("sedang download script")
+    urllib.request.urlretrieve(download_url, file_path)
+if os.path.exists(file_path):
+    spec = importlib.util.spec_from_file_location("firzah_enc", file_path)
+    fb = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(fb)
+    
